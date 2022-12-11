89d14152ad2cae125362d13449d3d7b23d2f5caf
Devil X - Sameer Khan <116139678+devilxsam@users.noreply.github.com>
2022-12-12 02:33:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,17 +5,17 @@ from translation import Translation
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = "5837023067:AAG3Qhl3LvQYwD3MDvtfmvsId4KTwCJABko"
+    TG_BOT_TOKEN = "5949491225:AAF2ZNOIGL8iQaPGubOcaM4q4PcnfG_54sQ"
     # required for running on Heroku
-    URL = "http://192.168.1.107"
+    URL = "http://192.168.1.106"
     PORT = int(os.environ.get("PORT", 5000))
     # Python3 ReQuests CHUNK SIZE
     CHUNK_SIZE = 10280
     # MyTelegram.org
     # configurtion required while creating new application
-    APP_TITLE = os.environ.get("APP_TITLE", "UseTGOrgBot")
-    APP_SHORT_NAME = os.environ.get("APP_SHORT_NAME", "UseTGOrgBot")
-    APP_URL = os.environ.get("APP_URL", "https://telegram.dog/UseTGOrgBot")
+    APP_TITLE = os.environ.get("APP_TITLE", "TM_MyTelegramOrgBot")
+    APP_SHORT_NAME = os.environ.get("APP_SHORT_NAME", "TM_MyTelegramOrgBot")
+    APP_URL = os.environ.get("APP_URL", "https://telegram.dog/TM_MyTelegramOrgBot")
     # these platform informations were obtained
     # on 27.01.2020 21:15:50 IST
     APP_PLATFORM = [
@@ -31,7 +31,7 @@ class Config(object):
     # if any of the platform, does not work
     # please reopen
     # https://github.com/LushaiTech/MyTelegramORG/issues/3
-    APP_DESCRIPTION = os.environ.get("APP_DESCRIPTION", "created using https://telegram.dog/UseTGOrgBot")
+    APP_DESCRIPTION = os.environ.get("APP_DESCRIPTION", "created using https://telegram.dog/TM_MyTelegramOrgBot")
     #
     FOOTER_TEXT = os.environ.get("FTEXT", "Please Join ❤️ @TechyBots")
     # the strings used in the different messages
